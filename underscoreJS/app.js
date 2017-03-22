22069646383b004666bf51ef6993e81bb73c05df
Anh Le <lequocanh88@gmail.com>
2017-03-22 20:59:28 +0700

    try to undersand _.filter of underscore.js

--- underscoreJS/app.js
+++ underscoreJS/app.js
@@ -5,3 +5,19 @@ console.log(arr6);
 
 var arr7 = _.filter([1,2,3,4,5,6,7], function(item) {return item%2 === 0;});
 console.log(arr7);
+
+
+_.filter = _.select = function(obj, predicate, context) {
+  var results = [];
+  predicate = cb(predicate, context);
+  _.each(obj, function(value, index, list) {
+    if (predicate(value, index, list)) results.push(value);
+  });
+  return results;
+};
+
+_.each(['Arguments', 'Function', 'String', 'Number', 'Date', 'RegExp', 'Error'], function(name) {
+  _['is' + name] = function(obj) {
+    return toString.call(obj) === '[object ' + name + ']';
+  };
+});
